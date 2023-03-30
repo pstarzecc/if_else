ef6761c5b7d38bb5f85afc34f154fa9ad624ecbf
Kacper <kacpergul13@gmail.com>
2023-03-30 13:51:59 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int time = 20;
+    if(time<15)
+    {
+        cout<<"dobry czas"<<endl;
+    }
+    else
+    {
+        cout<<"bledny czas"<<endl;
+    }
+    return 0;
+}
